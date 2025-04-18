c718d1876af6ea7ab7e9f9a666313a86f09af38d
KARTHIKEYAN <147401949+karthikeyan-2023@users.noreply.github.com>
2025-04-18 22:14:42 +0530

    Add files via upload

---
+++ air_line/jOptionPane.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.air_line;
+
+/**
+ *
+ * @author Karthikeyan
+ */
+class jOptionPane {
+
+    static void showMessageDialog(Object object, String customer_added_sussfully) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void showMessageDialog(String customer_added_sussfully) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
